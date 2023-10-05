cb380f9426ed26de89386d706e361d7f39527de2
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-10-05 07:42:12 -0500

    Initial Commit

---
+++ Displaying-Dowloaded-Image/Displaying_Dowloaded_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Displaying_Dowloaded_ImageApp.swift
+//  Displaying-Dowloaded-Image
+//
+//  Created by Aaron Brown on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Displaying_Dowloaded_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
